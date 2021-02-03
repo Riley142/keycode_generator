0c011f80e9c8399fc16e460cc2c0cc29c6a5b983
Riley <rileyxcroft@gmail.com>
2021-02-03 10:31:50 +0000

    Made the event listener for keycode display on the page

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
-// Get key & keycode
+const displayChar = document.querySelector("h2")
+
+
+// Display charcode & middle of the screen & event.wich
+
 document.addEventListener("keypress", event => {
-    console.log(event);
-})
+    let key = event.keyCode;
+    console.log(key);
+    displayChar.innerHTML = key;
+});
+
+
+
+// // need to get char code
+// save it as variable?
+// to be able to reuse iut and display it inside h2
+
+
+
+
+// Display key & event.key
+// Display keycode & event.code
+
+
+// Get key & keycode
+// document.addEventListener("keypress", event => {
+//     console.log(event);
+// })
+
+
+
